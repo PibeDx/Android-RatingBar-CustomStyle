7f7d71e69752d8bb9b6ebe0e54ed8b3f451aeb48
PibeDx <jcuentast@gmail.com>
2017-01-11 18:35:26 -0500

    commit initial

---
+++ app/src/main/java/com/josecuentas/android_ratingbar_customstyle/MainActivity.java
@@ -0,0 +1,13 @@
+package com.josecuentas.android_ratingbar_customstyle;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
